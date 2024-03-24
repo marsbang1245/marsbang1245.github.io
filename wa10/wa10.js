0e26adb97f0d7008101a924aa5c2dae0f4aa64b7
marsbang1245 <maju8002@colorado.edu>
2024-03-24 11:54:56 -0700

    wa10
    wa10

---
+++ wa10/wa10.js
@@ -0,0 +1,42 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = 'Under the moonlit sky, :insertx: decided to embark on a quest to :inserty:. Along the journey, they encountered :insertz:. Amazed by the encounter, :insertx: knew this adventure would be told for ages. And surely, when Bob heard about it, they were eager to join the next quest.';
+const insertX = ['Lorenzo the Bard', 'Mila the Ranger', 'Eldric the Wizard'];
+const insertY = ['the enchanted forest', 'the mysterious mountains', 'the ancient castle'];
+const insertZ = ['a band of wandering minstrels', 'a talking fox with secrets to share', 'an ancient scroll containing the spells of yore'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
